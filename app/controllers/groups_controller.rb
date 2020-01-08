513f7938d886453051491f6f41a26a7d3f24724d
kishidashota <59199947+kishidashota@users.noreply.github.com>
2020-01-08 17:01:08 +0900

    sending-message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,9 +2,13 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
-
   end
   
+
+  def edit
+   
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -21,7 +25,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -29,10 +33,18 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
+
   def set_group
     @group = Group.find(params[:id])
   end
 end
+
+
+
+  
+
+  
+
   
